e6b24e402af636f731b37c67f6c82de452529956
akarpov <akarpov@spotware.com>
2018-10-26 18:48:34 +0300

    rxjava

--- SpringBootAwesome/src/main/java/com/example/rpws/chapters/SpringBootAwesome/service/TemperatureSensor.java
+++ SpringBootAwesome/src/main/java/com/example/rpws/chapters/SpringBootAwesome/service/TemperatureSensor.java
@@ -1,36 +1,34 @@
 package com.example.rpws.chapters.SpringBootAwesome.service;
 
 import java.util.Random;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
-import javax.annotation.PostConstruct;
-
-import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Component;
 
 import com.example.rpws.chapters.SpringBootAwesome.domain.Temperature;
 
+import rx.Observable;
+
 @Component
 public class TemperatureSensor {
 
-    private final ApplicationEventPublisher publisher;
     private final Random rnd = new Random();
-    private final ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
 
-    public TemperatureSensor(ApplicationEventPublisher publisher) {
-        this.publisher = publisher;
+    private final Observable<Temperature> dataStream =
+            Observable
+                    .range(0, Integer.MAX_VALUE)
+                    .concatMap(tick -> Observable
+                            .just(tick)
+                            .delay(rnd.nextInt(5000), TimeUnit.MILLISECONDS)
+                            .map(tickValue -> this.probe()))
+                    .publish()
+                    .refCount();
+
+    private Temperature probe() {
+        return new Temperature(16 + rnd.nextGaussian() * 10);
     }
 
-    @PostConstruct
-    public void startProcessing() {
-        this.executor.schedule(this::probe, 1, TimeUnit.SECONDS);
-    }
-
-    private void probe() {
-        double temperature = 16 + rnd.nextGaussian() * 10;
-        publisher.publishEvent(new Temperature(temperature));
-        executor.schedule(this::probe, rnd.nextInt(5000), TimeUnit.MILLISECONDS);
+    public Observable<Temperature> temperatureStream() {
+        return dataStream;
     }
 }
