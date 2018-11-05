37e0d3238e22e669d55ab3c8a143921500d0e610
akarpov <akarpov@spotware.com>
2018-11-05 18:00:01 +0200

    Implementing custom subscribers

--- reactortest/src/main/java/com/example/Main.java
+++ reactortest/src/main/java/com/example/Main.java
@@ -1,14 +1,21 @@
 package com.example;
 
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
-
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.Optional;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.reactivestreams.Subscription;
+
+import reactor.core.Disposable;
+import reactor.core.publisher.BaseSubscriber;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.publisher.SignalType;
 
 public class Main {
 
-    public static void main(String... args) {
+    public static void main(String... args) throws Exception {
 
         Flux<String> stream1 = Flux.just("Hello", "world");
         Flux<Integer> stream2 = Flux.fromArray(new Integer[]{1, 2, 3});
@@ -34,7 +41,70 @@ public class Main {
                 .subscribe(
                         data -> System.out.println("onNext: " + data),
                         Throwable::printStackTrace,
-                        () -> System.out.println("onComplete"));
+                        () -> System.out.println("onComplete 1"));
+
+
+        //----------------
+
+        Flux.range(1, 100)
+                .subscribe(
+                        data -> System.out.println("onNext: " + data),
+                        Throwable::printStackTrace,
+                        () -> System.out.println("onCompete 2"),
+                        subscription -> {
+                            subscription.request(4);
+                            subscription.cancel();
+                        }
+                );
+        //----------------
+
+
+        BaseSubscriber<Long> customSubscriber = new BaseSubscriber<Long>() {
+            @Override
+            protected void hookOnComplete() {
+                System.out.println("OnComplete");
+            }
+
+            @Override
+            protected void hookOnSubscribe(Subscription subscription) {
+                super.hookOnSubscribe(subscription);
+                System.out.println("onSubscribe: " + subscription);
+            }
+
+            @Override
+            protected void hookOnNext(Long value) {
+                System.out.println("onNext: " + value);
+            }
+
+            @Override
+            protected void hookOnError(Throwable throwable) {
+                throwable.printStackTrace();
+            }
+
+            @Override
+            protected void hookOnCancel() {
+                System.out.println("onCancel");
+            }
+
+            @Override
+            protected void hookFinally(SignalType type) {
+                System.out.println("finally");
+            }
+        };
+
+        AtomicLong atomicLong = new AtomicLong();
+
+        Flux<Long> longFlux = Flux.interval(Duration.ofMillis(500)).map(i -> atomicLong.incrementAndGet());
+        longFlux.subscribe(customSubscriber);
+
+
+
+        Thread.sleep(3000);
+        Disposable disposable = longFlux.subscribe(System.out::println);
+        Thread.sleep(3000);
+        customSubscriber.dispose();
+        Thread.sleep(2000);
+        disposable.dispose();
 
 
     }
