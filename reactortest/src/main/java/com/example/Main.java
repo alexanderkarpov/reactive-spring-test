3c317ddbc32386b3de6e7c1eff772a3a130001b8
Alexander Karpov <alexander.karpoff@gmail.com>
2018-11-06 08:13:50 +0200

    Mapping elements of reactive sequences

--- reactortest/src/main/java/com/example/Main.java
+++ reactortest/src/main/java/com/example/Main.java
@@ -1,17 +1,18 @@
 package com.example;
 
-import java.time.Duration;
-import java.util.Arrays;
-import java.util.Optional;
-import java.util.concurrent.atomic.AtomicLong;
-
+import lombok.EqualsAndHashCode;
+import lombok.Value;
 import org.reactivestreams.Subscription;
-
 import reactor.core.Disposable;
 import reactor.core.publisher.BaseSubscriber;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.core.publisher.SignalType;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class Main {
 
@@ -23,7 +24,7 @@ public class Main {
         Flux<Integer> stream4 = Flux.range(2018, 10);
 
         Mono<String> stream5 = Mono.just("One");
-        Mono<String> stream6 = Mono.justOrEmpty(null);
+//        Mono<String> stream6 = Mono.justOrEmpty(null);
         Mono<String> stream7 = Mono.justOrEmpty(Optional.empty());
 
         stream5.subscribe(System.out::println);
@@ -58,56 +59,74 @@ public class Main {
                 );
         //----------------
 
-
-        BaseSubscriber<Long> customSubscriber = new BaseSubscriber<Long>() {
-            @Override
-            protected void hookOnComplete() {
-                System.out.println("*** OnComplete");
-            }
-
-            @Override
-            protected void hookOnSubscribe(Subscription subscription) {
-                System.out.println("*** onSubscribe: " + subscription);
-                request(1);
-            }
-
-            @Override
-            protected void hookOnNext(Long value) {
-                request(1);
-                System.out.println("*** onNext: " + value);
-            }
-
-            @Override
-            protected void hookOnError(Throwable throwable) {
-                throwable.printStackTrace();
-            }
-
-            @Override
-            protected void hookOnCancel() {
-                System.out.println("*** onCancel");
-            }
-
-            @Override
-            protected void hookFinally(SignalType type) {
-                System.out.println("*** finally");
-            }
-        };
+        CustomSubscriber<Long> customSubscriber = new CustomSubscriber<>(1, 0);
 
         AtomicLong atomicLong = new AtomicLong();
 
-        Flux<Long> longFlux = Flux.interval(Duration.ofMillis(500)).map(i -> atomicLong.incrementAndGet());
+        Flux<Long> longFlux = Flux.interval(Duration.ofMillis(50)).map(i -> atomicLong.incrementAndGet());
         longFlux.subscribe(customSubscriber);
 
 
-
-        Thread.sleep(3000);
+        Thread.sleep(300);
         Disposable disposable = longFlux.subscribe(System.out::println);
-        Thread.sleep(3000);
+        Thread.sleep(300);
         customSubscriber.dispose();
-        Thread.sleep(2000);
+        Thread.sleep(200);
         disposable.dispose();
 
+        System.out.println("----------------");
+        //  https://projectreactor.io/docs/core/release/reference/#which-operator
+        //----------------
+        Flux.range(2018, 5)
+                .timestamp()
+                .index()
+                .subscribe(new CustomSubscriber<>(1, 777));
 
+    }
+
+    @EqualsAndHashCode(callSuper = true)
+    @Value
+    private static class  CustomSubscriber<T> extends BaseSubscriber<T> {
+
+        long request;
+        long processingTimeout;
+
+        @Override
+        protected void hookOnComplete() {
+            System.out.println("*** OnComplete");
+        }
+
+        @Override
+        protected void hookOnSubscribe(Subscription subscription) {
+            System.out.println("*** onSubscribe: " + subscription);
+            request(request);
+        }
+
+        @Override
+        protected void hookOnNext(T value) {
+            System.out.println("*** onNext: " + value);
+            try {
+                Thread.sleep(processingTimeout);
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+            request(request);
+        }
+
+        @Override
+        protected void hookOnError(Throwable throwable) {
+            throwable.printStackTrace();
+        }
+
+        @Override
+        protected void hookOnCancel() {
+            System.out.println("*** onCancel");
+        }
+
+        @Override
+        protected void hookFinally(SignalType type) {
+            System.out.println("*** finally");
+        }
     }
 
 
